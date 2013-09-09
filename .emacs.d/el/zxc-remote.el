4205b8d208d0bcaf03d9df30664ec5d51fc0887c
david <david.c.aq@gmail.com>
2013-09-09 16:53:10 +0800

    add new alias

--- .emacs.d/el/zxc-remote.el
+++ .emacs.d/el/zxc-remote.el
@@ -4,6 +4,8 @@
 (require 'drkm-fav)
 (setq drkm-fav:favourite-directories-alist
       '(("aiods" . "/aiods@10.109.1.8:/export/home/aiods")
-	("aiapp" . "/aiapp@10.109.1.8:/export/home/aiapp")))
+	("aiapp" . "/aiapp@10.109.1.8:/export/home/aiapp")
+	("core" . "~/work/wuhan/core")
+	("158" . "/db2inst1@10.95.239.158:~/")))
 
 (provide 'zxc-remote)
