93ae3f9c4e96320c85cd1f39c3313e08d7e5df1b
LY <aq136_a00@foxmail.com>
2022-11-01 17:38:17 +0800

    feat: 增加eaf插件

---
+++ .emacs.d/el/zxc-eaf.el
@@ -0,0 +1,9 @@
+(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
+
+(require 'eaf)
+(require 'eaf-file-sender)
+;; (require 'eaf-image-viewer)
+;; (require 'eaf-mindmap)
+(require 'eaf-airshare)
+
+(provide 'zxc-eaf)
